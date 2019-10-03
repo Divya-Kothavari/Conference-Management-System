f4f7400e25df2f4d871abe0bb4e01943a81c7ff7
divya-kothavari <divyak13@gmail.com>
2019-10-04 00:30:27 +0530

    frebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDfXrvhvN04v_fC7yJyfh0ZKdLUshqdsd4',
-    authDomain: 'jresearchvalley-5d1de.firebaseapp.com',
-    databaseURL: 'https://jresearchvalley-5d1de.firebaseapp.com/',
-    projectId: 'jresearchvalley-5d1de',
-    storageBucket: 'us',
-    messagingSenderId: '860271312023'
+    apiKey: "AIzaSyDfXrvhvN04v_fC7yJyfh0ZKdLUshqdsd4",
+    authDomain: "jresearchvalley-5d1de.firebaseapp.com",
+    databaseURL: "https://jresearchvalley-5d1de.firebaseio.com",
+    projectId: "jresearchvalley-5d1de",
+    storageBucket: "jresearchvalley-5d1de.appspot.com",
+    messagingSenderId: "860271312023",
+    appId: "1:860271312023:web:aac8b768dc1bff92a8fdd2",
+    measurementId: "G-D4646083K5"
   }
 };
 
